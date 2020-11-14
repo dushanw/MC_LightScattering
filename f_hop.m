93a903dd3eca9104c8e81416a4106874ddcdb64c
Dushan N. Wadduwage <15632687+dushanw@users.noreply.github.com>
2020-11-14 12:44:58 +0530

    random number generation in gpu

--- f_hop.m
+++ f_hop.m
@@ -2,7 +2,13 @@
 
 function [x, y, z, L, atSurf] = f_hop(x,y,z,ux,uy,uz,L,pram)
 
-  s         = 1e4 * (-log(rand(pram.Nphotons,1))/pram.mus); % [um]      Step size.  Note: log() is base e
+  if pram.useGpu == 0
+    rnd1      = rand(pram.Nphotons,1);
+  else
+    rnd1      = rand(pram.Nphotons,1,"gpuArray");
+  end
+
+  s         = 1e4 * (-log(rnd1)/pram.mus); % [um]      Step size.  Note: log() is base e
   
   atSurf    = find(z + s .* uz >= 0);                       %           propergate only to the
   s(atSurf) = - z(atSurf)./uz(atSurf); 
--- f_spin.m
+++ f_spin.m
@@ -1,10 +1,18 @@
 
 function [ux, uy, uz] = f_spin(ux,uy,uz,atSurf,pram)
-  
-  temp      = (1 - pram.g*pram.g)./(1 - pram.g + 2*pram.g*rand(pram.Nphotons,1));
+
+  if pram.useGpu == 0
+    rnd1      = rand(pram.Nphotons,1);
+    rnd2      = rand(pram.Nphotons,1);
+  else
+    rnd1      = rand(pram.Nphotons,1,"gpuArray");
+    rnd2      = rand(pram.Nphotons,1,"gpuArray");
+  end
+
+  temp      = (1 - pram.g*pram.g)./(1 - pram.g + 2*pram.g* rnd1);
   costheta  = (1 + pram.g*pram.g - temp.*temp)/(2 * pram.g);
   sintheta  = sqrt(1 - costheta.*costheta);
-  psi       = rand(pram.Nphotons,1)*2*pi;
+  psi       = rnd2 * 2*pi;
      
   temp      = sqrt(1.0 - uz .* uz);
   uxx       =  sintheta .* (ux .* uz .* cos(psi) - uy .* sin(psi)) ./ temp + ux .* costheta;
