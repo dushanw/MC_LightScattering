c212c0e55906133a57cdb78eac9a8a56080c2594
Dushan N. Wadduwage <15632687+dushanw@users.noreply.github.com>
2020-11-16 12:44:51 +0530

    minor change to f_cat

--- f_catAllSims.m
+++ f_catAllSims.m
@@ -1,5 +1,12 @@
 function [x, y, z, ux, uy, uz] = f_catAllSims(x,y,z,ux,uy,uz,pram)
 
+  x     = cat(1,x {:});
+  y     = cat(1,y {:});
+  z     = cat(1,z {:});
+  ux    = cat(1,ux{:});
+  uy    = cat(1,uy{:});
+  uz    = cat(1,uz{:});
+  
   if pram.useGpu == 1
     x   = gather(x );
     y   = gather(y );
@@ -9,11 +16,4 @@ function [x, y, z, ux, uy, uz] = f_catAllSims(x,y,z,ux,uy,uz,pram)
     uz  = gather(uz);
   end
 
-  x     = cat(1,x {:});
-  y     = cat(1,y {:});
-  z     = cat(1,z {:});
-  ux    = cat(1,ux{:});
-  uy    = cat(1,uy{:});
-  uz    = cat(1,uz{:});
-  
 end
