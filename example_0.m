e1839edaf6fd62613dff81b353b87eddfe3575d0
Dushan N. Wadduwage <15632687+dushanw@users.noreply.github.com>
2021-04-09 12:25:50 +0530

    mf

--- example_0.m
+++ example_0.m
@@ -3,10 +3,10 @@
 
 pram = f_praminit();  % initializes all paramteres. Take a look at 'f_praminit.m' for details
 
-pram.z0_um    = -300;
+pram.z0_um    = -100;
 pram.Nphotons = 100000;
 main(pram);           % runs simulation for parameters in 'pram' ...
                       % and saves results to folder defined by 'pram.savepath' 
                       
 % load sPSF.mat
-f_plotResults(sPSF,sPSF_axis,x_backProp,y_backProp,pram);
+% f_plotResults(sPSF,sPSF_axis,x_backProp,y_backProp,pram);
